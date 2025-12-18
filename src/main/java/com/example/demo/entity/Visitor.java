d5f1f4a760499ba509d1aeb60918ef318ed966d8
bala-amypo <bala@amypo.in>
2025-12-18 05:25:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/Visitor.java
+++ src/main/java/com/example/demo/entity/Visitor.java
@@ -1,9 +1,30 @@
-public class Visitor{
-    private Long id;
-    private String fullName;
-    private String email;
-    private String phone;
-    private String idProof;
-    private LocalDateTime createdAt;
-    
+package com.example.demo.model;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Visitor {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private String fullName;
+private String email;
+private String phone;
+private String idProof;
+private LocalDateTime createdAt;
+
+
+@PrePersist
+void prePersist() {
+if (phone == null || phone.isBlank()) {
+throw new IllegalArgumentException("phone required");
 }
+createdAt = LocalDateTime.now();
+}
+}
+
