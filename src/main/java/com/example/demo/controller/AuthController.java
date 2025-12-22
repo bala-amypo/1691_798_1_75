0117a5f8eb7d30c64394b6bb3c5df1594b1572cb
bala-amypo <bala@amypo.in>
2025-12-22 05:52:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,10 +1,9 @@
 package com.example.demo.controller;
 
-// import com.example.demo.dto.AuthRequest;
-// import com.example.demo.dto.AuthResponse;
-// import com.example.demo.dto.RegisterRequest;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.RegisterRequest;
 import com.example.demo.service.UserService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +12,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@Tag(name = "Authentication")
 public class AuthController {
 
     private final UserService userService;
 
     @PostMapping("/register")
-    // public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
-    //     return new ResponseEntity<>(userService.register(request), HttpStatus.CREATED);
-    // }
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        return new ResponseEntity<>(userService.register(request), HttpStatus.CREATED);
+    }
 
     @PostMapping("/login")
-    // public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-    //     return ResponseEntity.ok(userService.login(request));
-    // }
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(userService.login(request));
+    }
 }
