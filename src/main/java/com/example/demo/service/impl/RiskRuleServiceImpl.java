e358b4ee0338f8e4d4e364570547650f5ae4e090
bala-amypo <bala@amypo.in>
2025-12-22 05:17:04 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskRuleServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.model.RiskRule;
+// import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.entity.RiskRule;
 import com.example.demo.repository.RiskRuleRepository;
 import com.example.demo.service.RiskRuleService;
 import lombok.RequiredArgsConstructor;
